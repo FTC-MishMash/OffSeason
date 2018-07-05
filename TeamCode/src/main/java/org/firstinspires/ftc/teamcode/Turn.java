6f274a5562425fdc11d2de5e917bc85bd8e5e4ca
Mor1000 <33144215+Mor1000@users.noreply.github.com>
2018-07-05 18:31:11 +0300

    33

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turn.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -7,37 +9,58 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 /**
  * Created by user on 18/06/2018.
  */
-
+@Autonomous(name = "TurnTrial")
 public class Turn extends NewGlobal {
     @Override
     public void runOpMode() throws InterruptedException {
-
+        super.runOpMode();
+        waitForStart();
+        double angleTurn = 0;
+        while (opModeIsActive())
+        {
+            telemetry.addData("angle: ",angleTurn);
+            telemetry.update();
+            if (gamepad1.dpad_up){
+                angleTurn += 1;
+            sleep(100);}
+            if (gamepad1.dpad_down){
+                angleTurn -= 1;
+                sleep(100);
+            }
+            if (gamepad1.y){
+                angleTurn *= -1;
+                sleep(100);
+            }
+            if (gamepad1.a)
+                Turn(angleTurn, 0.4);
+        }
+        // Turn(-20, 0.4);
     }
 
-    public void Turn(double goalAngle) {
+    public void Turn(double goalAngle, double power) {
         // check the side of turn (left or right)
 
-        boolean sideOfTurn = true;//true = turn right, false = turn left
+        boolean sideOfTurn = true;//true = turn clockwise, false = turn counter-clockwise
         if (goalAngle == 0) {
             if (imu.getAngularOrientation(AxesReference.INTRINSIC,
                     AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle < 0) {
-                //  sideOfTurn= true??;    //right
+                sideOfTurn = false;    //right
             } else if (imu.getAngularOrientation(AxesReference.INTRINSIC,
                     AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle > 0) {
-                //  sideOfTurn= false??;    //left
+                sideOfTurn = true;    //left
             }
         }
         if (imu.getAngularOrientation(AxesReference.INTRINSIC,
                 AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle <= 0 && goalAngle < 0 &&
                 imu.getAngularOrientation(AxesReference.INTRINSIC,
                         AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle < goalAngle) {
-            //  sideOfTurn= true??;     //right
+            sideOfTurn = false;     //right
 
         } else if (imu.getAngularOrientation(AxesReference.INTRINSIC,
                 AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle <= 0 && goalAngle < 0
                 && imu.getAngularOrientation(AxesReference.INTRINSIC,
                 AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle > goalAngle) {
-            //  sideOfTurn= false??;    //left
+            sideOfTurn = true;    //left
 
         }
         if (imu.getAngularOrientation(AxesReference.INTRINSIC,
@@ -46,13 +69,13 @@ public class Turn extends NewGlobal {
                         AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle < goalAngle)
 
         {
-            //  sideOfTurn= true??;    //right
+            sideOfTurn = false;    //right
 
         } else if (imu.getAngularOrientation(AxesReference.INTRINSIC,
                 AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle >= 0 && goalAngle > 0 &&
                 imu.getAngularOrientation(AxesReference.INTRINSIC,
                         AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle > goalAngle) {
-            //  sideOfTurn= false??;    //left
+            sideOfTurn = true;    //left
 
         } else if (imu.getAngularOrientation(AxesReference.INTRINSIC,
                 AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle >= 0 && goalAngle < 0)
@@ -60,11 +83,11 @@ public class Turn extends NewGlobal {
         {
             if ((imu.getAngularOrientation(AxesReference.INTRINSIC,
                     AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - goalAngle) >
-                    (180 - imu.getAngularOrientation(AxesReference.INTRINSIC,
-                            AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) + 180 + goalAngle) {
-                //  sideOfTurn= true???;    //right
+                    (360 - imu.getAngularOrientation(AxesReference.INTRINSIC,
+                            AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) + goalAngle) {
+                sideOfTurn = false;    //right
             } else {
-//  sideOfTurn= false???;    //left
+                sideOfTurn = true;    //left
             }
         } else if (imu.getAngularOrientation(AxesReference.INTRINSIC,
                 AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle <= 0 && goalAngle > 0)
@@ -72,25 +95,48 @@ public class Turn extends NewGlobal {
         {
             if ((-imu.getAngularOrientation(AxesReference.INTRINSIC,
                     AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle + goalAngle) <
-                    (180 + imu.getAngularOrientation(AxesReference.INTRINSIC,
-                            AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle + 180 - goalAngle)) {
-                //  sideOfTurn= true???;    //right
+                    (360 + imu.getAngularOrientation(AxesReference.INTRINSIC,
+                            AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - goalAngle)) {
+                sideOfTurn = false;    //right
             } else {
-                //  sideOfTurn= true???;    //right
+                sideOfTurn = true;    //left
             }
 
-        }//0
+        }
         if (sideOfTurn) {
-            while (goalAngle < imu.getAngularOrientation(AxesReference.INTRINSIC,
-                    AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) {
-                //motors running
-            }
+            setMotorPower(new double[][]{{power, -power}, {power, -power}});
+            if (goalAngle > 0 && imu.getAngularOrientation(AxesReference.INTRINSIC,
+                    AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle < 0)
 
-        } else {
-            while (goalAngle > imu.getAngularOrientation(AxesReference.INTRINSIC,
-                    AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) {
+                while (goalAngle > imu.getAngularOrientation(AxesReference.INTRINSIC,
+                        AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) ;
                 //motors running
+
+            else {
+
+                while (goalAngle < imu.getAngularOrientation(AxesReference.INTRINSIC,
+                        AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) {
+                    telemetry.addData("angle:", imu.getAngularOrientation(AxesReference.INTRINSIC,
+                            AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
+                    telemetry.update();
+                }
+
+                //motors running
+
             }
+        } else {
+            setMotorPower(new double[][]{{-power, power}, {-power, power}});
+            if (goalAngle < 0 && imu.getAngularOrientation(AxesReference.INTRINSIC,
+                    AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle > 0)
+                while (goalAngle < imu.getAngularOrientation(AxesReference.INTRINSIC,
+                        AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) ;
+                //motors running
+
+            else
+                while (goalAngle > imu.getAngularOrientation(AxesReference.INTRINSIC,
+                        AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle) ;
+            //motors running
+
         }
         setMotorPower(new double[][]{{0, 0}, {0, 0}});
     }
